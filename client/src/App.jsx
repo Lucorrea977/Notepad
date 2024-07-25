bb6c5e25009622192d253a86492132131a670b9c
Lucorrea977 <luucamila6@gail.com>
2024-07-25 16:27:37 -0300

    delete archivos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import TaskManager from './views/TaskManager';
-import Login from './views/Login';
-import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/tasks"
-        element={
-          <PrivateRoute>
-            <TaskManager />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/" element={<TaskManager />} />
+    <Route path="/" element={<TaskManager />} />
     </Routes>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './firebase.config'; 
+
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import './assets/main.css';
